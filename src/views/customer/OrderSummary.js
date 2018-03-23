d6d27119a42c6720562a6f77d5650388183fd90f
fuadinaqi <fuadinaqi@aisifoundation.org>
2018-03-23 15:53:08 +0700

    UI hampir done

--- src/views/customer/OrderSummary.js
+++ src/views/customer/OrderSummary.js
@@ -1,8 +1,29 @@
-import React, { Component } from 'react'
-
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Body, Text, Left, Right, Button } from 'native-base';
 
 class OrderSummary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        orderLists: [
+          {
+            menuId: 'Indomie Carbonara',
+            quantity: 3,
+            price: 22000
+          },{
+            menuId: 'Sushimie',
+            quantity: 2,
+            price: 18000
+          }, {
+            menuId: 'ketigadsfad',
+            quantity: 1,
+            price: 12000
+          }
+        ]
+    };
+
+  }
 
   static navigationOptions = ({ navigation }) => {
     const name = 'Order Summary'
@@ -13,12 +34,94 @@ class OrderSummary extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Summary List Here !</Text>
-      </View>
-    )
+      <Content>
+        <Card>
+          <CardItem bordered>
+            <Text style={{fontWeight: 'bold'}}>
+              Items to order
+            </Text>
+          </CardItem>
+          { this.state.orderLists.map( list => {
+            return (
+              <CardItem bordered style={{flex: 1, flexWrap: 'wrap'}}>
+                <View key={list} style={styles.container}>
+                  <Left>
+                    <Text>
+                      {list.menuId}
+                    </Text>
+                  </Left>
+                  <Right>
+                    <Text>
+                      {list.price}
+                    </Text>
+                  </Right>
+                </View>
+                <View style={styles.container}>
+                  <Left>
+                  </Left>
+                  <Right>
+                    <View style={{flex: 1, flexDirection:'row'}}>
+                      <Button 
+                        style={styles.button} bordered warning
+                        onPress={() => {
+                          // TODO: ini minus nya ya
+                          this.setState({
+                            menuId: list.menuId,
+                            quantity: list.quantity--
+                          })
+                        }}
+                        >
+                        <Text style={styles.insideButton}>-</Text>
+                      </Button>
+                      <Button style={styles.button} bordered warning>
+                        <Text style={styles.insideButton}>{list.quantity}</Text>
+                      </Button>
+                      <Button 
+                        style={styles.button} bordered warning
+                        onPress={() => {
+                          this.setState({
+                            menuId: list.menuId,
+                            quantity: list.quantity++
+                          })
+                        }}
+                      >
+                        <Text style={styles.insideButton}>+</Text>
+                      </Button>
+                    </View>
+                  </Right>
+                </View>
+              </CardItem>
+            )
+          })}
+          
+        </Card>
+        <View>
+          <Button style={styles.submit} block warning>
+            <Text> Order </Text>
+          </Button>
+        </View>
+      </Content>
+    );
   }
-
 }
 
-export default OrderSummary
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  insideButton: {
+    fontSize: 14,
+    width: 60
+  },
+  button: {
+    height: 35,
+    width: 35,
+    justifyContent: 'flex-start'
+  },
+  submit: {
+    margin: 5,
+    alignItems: 'center'
+  }
+});
+
+export default OrderSummary;
